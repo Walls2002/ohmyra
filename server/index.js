3bcbe6f13f3e0315e145d3a5914286a46f3f0551
Wally2002 <walliegaynor29@gmail.com>
2024-09-27 16:30:56 +0800

    fix: test urls(2)

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.PROD_HOST,
+    origin: "https://ohmyra.vercel.app",
     methods: ["GET", "POST"],
     credentials: true,
   },
